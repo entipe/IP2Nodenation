1fb56663403d4a11ed68058f090bfc9984a25a8e
Gauthier <gauthier.depaoli@gmail.com>
2022-01-29 16:07:14 +0100

    Create an Express app to retrieve user country code from ip

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const requestIp = require('request-ip');
+const {getClientIp} = require("request-ip");
+const cors = require('cors');
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const domainWhiteList = process.env.DOMAIN_WHITELIST.split(',');
+const app = express();
+
+app.use(requestIp.mw());
+app.use(cors({
+  origin: function(origin, callback) {
+    if(!origin || domainWhiteList.some('*')) return callback(null, true);
+
+    if(domainWhiteList.some(origin)) {
+      return callback(new Error('401'), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+app.get('/api/ip2nation', (req, res) => {
+  const apiKey = req.query.apiKey;
+
+  const geoip = require('@avindak/xgeoip');
+
+  geoip.load_memory().then(_ => {
+    geoip.resolve(getClientIp(req)).then(r => {
+      res.send({country: r.country_code});
+    }).catch(e => {
+      res.send({country: ''});
+    })
+  });
+});
+
+app.listen(8080);
